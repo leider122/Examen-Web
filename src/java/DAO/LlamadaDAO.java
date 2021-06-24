a0e1fc91b277f5e29f246ac9c9956a9786e15bad
Leider Yesid Martinez Mandon <leideryesidmm@ufps.edu.co>
2021-06-24 08:56:34 -0500

    TERCER COMMIT

---
+++ src/java/DAO/LlamadaDAO.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import DTO.Llamada;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import persistencia.LlamadaJpaController;
+import persistencia.exceptions.NonexistentEntityException;
+
+/**
+ *
+ * @author USER
+ */
+public class LlamadaDAO {
+    LlamadaJpaController l=new LlamadaJpaController();
+    public void create(Llamada llam){
+        try {
+            l.create(llam);
+        } catch (Exception ex) {
+            Logger.getLogger(LlamadaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public List<Llamada> read(){
+    return l.findLlamadaEntities();
+    }
+    
+    public Llamada readDpt(int cod){
+        return l.findLlamada(cod);
+    }
+    
+    public void update(Llamada llam){
+        try {
+            l.edit(llam);
+        } catch (Exception ex) {
+            Logger.getLogger(LlamadaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    } 
+    
+    public void delete(int cod){
+        try {
+            l.destroy(cod);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(LlamadaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ src/java/DAO/PaisDAO.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import DTO.Pais;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import persistencia.PaisJpaController;
+import persistencia.exceptions.IllegalOrphanException;
+import persistencia.exceptions.NonexistentEntityException;
+
+/**
+ *
+ * @author USER
+ */
+public class PaisDAO {
+    PaisJpaController pa=new PaisJpaController();
+    public void create(Pais d){
+        try {
+            pa.create(d);
+        } catch (Exception ex) {
+            Logger.getLogger(PaisDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public List<Pais> read(){
+    return pa.findPaisEntities();
+    }
+    
+    public Pais readDpt(int cod){
+        return pa.findPais(cod);
+    }
+    
+    public void update(Pais dpar){
+        try {
+            pa.edit(dpar);
+        } catch (Exception ex) {
+            Logger.getLogger(PaisDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    } 
+    
+    public void delete(int cod) throws IllegalOrphanException{
+        try {
+            pa.destroy(cod);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(PaisDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
