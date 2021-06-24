264d8037bf498e35e3942f27240ecc20eef55970
Leider Yesid Martinez Mandon <leideryesidmm@ufps.edu.co>
2021-06-24 10:25:20 -0500

    QUINTO COMMIT

--- src/java/negocio/AnalizadorLlamadas.java
+++ src/java/negocio/AnalizadorLlamadas.java
@@ -5,8 +5,12 @@
  */
 package negocio;
 
+import DAO.LlamadaDAO;
 import DAO.UsuarioDAO;
+import DTO.Llamada;
 import DTO.Usuario;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -23,12 +27,59 @@ public class AnalizadorLlamadas {
     }
 
     public String getTablaUsuario(Usuario u) {
-        String tabla="";
-        return tabla;
+        String tabla="<table>"+
+                "<tr><th>Nombre</th>"+
+                "<th>Email</th>"+
+                "<th>Foto</th><tr>"+
+                "<tr><td>"+u.getNombre()+"</td>"+
+                "<td>"+u.getEmail()+"</td>"+
+                "<td><img src='"+u.getFoto()+"'></td></tr>"+
+                "";
+        return tabla+"</tabla>";
     }
 
     public String getTablaLlamadas(Usuario u) {
-        String tabla="";
-        return tabla;
+        String llamadas=getTLlama(u);
+        String tabla="<table>"+
+                "<tr><th>Pais</th>"+
+                "<th>Min inicial</th>"+
+                "<th>Min finth>"+
+                "<th>Valor minuto</th>"+
+                "<th>Valor Llamada</th><tr>";
+        
+        return tabla+llamadas+"</table>";
+    }
+    public List<Llamada> llamadasPersona(Usuario u){
+        List<Llamada> l=new ArrayList();
+        
+        LlamadaDAO ll=new LlamadaDAO();
+        List<Llamada> l2=ll.read();
+        for(Llamada lm:l2){
+            if(lm.getCedula().equals(u)){
+               l.add(lm);
+            }
+        }
+        return l;
+    }
+    public String getTLlama(Usuario u){
+        List<Llamada> l=llamadasPersona(u);
+        String msg="";
+        
+        for(Llamada ll:l){
+            msg+="<tr><td>"+ll.getIdPais().getNombre()+"</td>"+
+                    "<td>"+ll.getMinInicio()+"</td>"+
+                    "<td>"+ll.getMinFin()+"</td>"+
+                    "<td>"+ll.getIdPais().getCostoMInuto()+"</td>"+
+                    "<td>"+getCostoLlamada(ll.getMinInicio(),ll.getMinFin(),ll.getIdPais().getCostoMInuto())+"</td></tr>";
+        }
+        
+        return msg;
+    }
+    public long getCostoLlamada(long minIni,long minFin,long costomin){
+        long cantidadmin=minFin-minIni;
+        if(cantidadmin<=0){
+        return 0;
+        }
+        return (cantidadmin)*costomin;
     }
 }
--- web/jsp/tablas.jsp
+++ web/jsp/tablas.jsp
@@ -12,6 +12,19 @@
         <title>JSP Page</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <div>
+            <h1>Analizador de llamadas</h1>
+            <h5>Desarrollado por: Leider Yesid Martinez Mandon- Codigo: 1151786 </h5>
+            <hr><br>
+        <form action="Tabla.do">
+            <h3>Digite cédula del suscriptor:</h3>
+        <input type="number" name="cedula" value="0">
+        
+        <input type="submit" name="boton" value="Consultar Factura">
+        </form>
+        </div>
+        <footer>
+    	<h5 class="text-center" style="margin-top: 60px;">UFPS-2021-Programación web- UFPS</h5>
+</footer>
     </body>
 </html>
